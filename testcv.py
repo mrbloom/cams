e427229c4e87c481d1cfe1e5eba35aca11b180f7
mrbloom <mrbloom@ukr.net>
2019-11-24 23:20:30 +0200

    2nd commit

--- testcv.py
+++ testcv.py
@@ -1,13 +1,19 @@
+import numpy as np
 import cv2
+from matplotlib import pyplot as plt
 
 cam_urls = ['http://192.168.88.233:8080/video','http://192.168.88.231:8080/video']
 
 caps = [cv2.VideoCapture(cam_url) for cam_url in cam_urls]
 
-while(True):
-    for i,cap in enumerate(caps):
-        ret, frame = cap.read()
-        cv2.imshow(f'frame {i}',frame)
+while(True):        
+    imgL,imgR = [cap.read()[1] for cap in caps]
+
+    stereo = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(imgL,imgR)
+    plt.imshow(disparity,'gray')
+    plt.show()
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
